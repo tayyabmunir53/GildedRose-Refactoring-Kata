1b10aba7bb8da8c632a007ddfa4dd8740b663ff2
Tayyab Munir <muhammad.munir@ericsson.com>
2022-12-02 16:16:40 +0100

    [Refactoring] extracted methods for update quality and sellIn

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,51 +13,63 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BERRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURUS)) {
-                        items[i].quality = items[i].quality - 1;
+            updateQuality(i);
+
+            updateSellin(i);
+
+            updateQualityWhenSellingDatePassed(i);
+        }
+    }
+
+    private void updateQualityWhenSellingDatePassed(int i) {
+        if (items[i].sellIn < 0) {
+            if (!items[i].name.equals(AGED_BERRIE)) {
+                if (!items[i].name.equals(BACKSTAGE_PASSES)) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals(SULFURUS)) {
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
+                } else {
+                    items[i].quality = items[i].quality - items[i].quality;
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
                 }
             }
+        }
+    }
 
-            if (!items[i].name.equals(SULFURUS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+    private void updateSellin(int i) {
+        if (!items[i].name.equals(SULFURUS)) {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
+    }
+
+    private void updateQuality(int i) {
+        if (!items[i].name.equals(AGED_BERRIE)
+                && !items[i].name.equals(BACKSTAGE_PASSES)) {
+            if (items[i].quality > 0) {
+                if (!items[i].name.equals(SULFURUS)) {
+                    items[i].quality = items[i].quality - 1;
+                }
             }
+        } else {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BERRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURUS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                if (items[i].name.equals(BACKSTAGE_PASSES)) {
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
                     }
                 }
             }
