52e5ad8e40c48d6d23cf98ea9d96cf344cfb36c4
Tayyab Munir <muhammad.munir@ericsson.com>
2022-12-02 16:09:18 +0100

    [Refactoring] made product names in static interface

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,9 @@
 package com.gildedrose;
 
+import static com.gildedrose.ProductNames.AGED_BERRIE;
+import static com.gildedrose.ProductNames.BACKSTAGE_PASSES;
+import static com.gildedrose.ProductNames.SULFURUS;
+
 class GildedRose {
     Item[] items;
 
@@ -9,10 +13,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BERRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURUS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +24,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +40,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURUS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(AGED_BERRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(SULFURUS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
---
+++ Java/src/main/java/com/gildedrose/ProductNames.java
@@ -0,0 +1,11 @@
+package com.gildedrose;
+
+public interface ProductNames {
+    String AGED_BERRIE = "Aged Brie";
+    String SULFURUS = "Sulfuras, Hand of Ragnaros";
+    String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
+
+
+
+}
